dd0ffee1fa58bc7992b8ad7619dac45a5c5492da
Hritik Bagane <hritikbagane@gmail.com>
2020-10-01 00:10:52 +0530

    Done it via HashSet

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,9 +1,16 @@
 class Solution {
     public void reverseString(char[] s) {
         
-		s.reverse();
-		System.out.println(s);
+//         i →               ← j
+//         h    e    l    l    o                   
 
+//         exchange i with j in each iteration.
+        
+        int i = 0, j = s.length - 1;
+        while (i < j) {
+            char t = s[i];
+            s[i++] = s[j];
+            s[j--] = t;
         }
     }
 }
